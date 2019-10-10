6216d6e11a845c0b7b0414516dc27cafd0ed49da
Igor Spichkin <igorsa.ux@gmail.com>
2019-10-10 16:23:20 +0300

    AirlockElectronics ui

--- tgui-next/packages/tgui/interfaces/AirlockElectronics.js
+++ tgui-next/packages/tgui/interfaces/AirlockElectronics.js
@@ -57,54 +57,6 @@ export const AirlockElectronics = props => {
               onClick={() => act(ref, 'one_access')}
             />
           </LabeledList.Item>
-          <LabeledList.Item
-            label="Mass Modify">
-            <Button
-              icon="check-double"
-              content="Grant All"
-              onClick={() => act(ref, 'grant_all')}
-            />
-            <Button
-              icon="undo"
-              content="Clear All"
-              onClick={() => act(ref, 'clear_all')}
-            />
-          </LabeledList.Item>
-          <LabeledList.Item
-            label="Unrestricted Access">
-            <Button
-              icon={data.unres_direction & 1 ? 'check-square-o' : 'square-o'}
-              content="North"
-              selected={data.unres_direction & 1}
-              onClick={() => act(ref, 'direc_set', {
-                unres_direction: '1',
-              })}
-            />
-            <Button
-              icon={data.unres_direction & 2 ? 'check-square-o' : 'square-o'}
-              content="East"
-              selected={data.unres_direction & 2}
-              onClick={() => act(ref, 'direc_set', {
-                unres_direction: '2',
-              })}
-            />
-            <Button
-              icon={data.unres_direction & 4 ? 'check-square-o' : 'square-o'}
-              content="South"
-              selected={data.unres_direction & 4}
-              onClick={() => act(ref, 'direc_set', {
-                unres_direction: '4',
-              })}
-            />
-            <Button
-              icon={data.unres_direction & 8 ? 'check-square-o' : 'square-o'}
-              content="West"
-              selected={data.unres_direction & 8}
-              onClick={() => act(ref, 'direc_set', {
-                unres_direction: '8',
-              })}
-            />
-          </LabeledList.Item>
         </LabeledList>
       </Section>
       <Section title="Access">
