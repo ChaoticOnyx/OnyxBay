0e373368a2f0c538982398d3423d67de9706cb85
igorsaux <55196698+igorsaux@users.noreply.github.com>
2021-05-06 23:29:31 +0300

    Changelog Generation (#4965)
    - No more manual writing.
    - Yaaay!

---
+++ tools/ChangelogGenerator/Fetch.ps1
@@ -0,0 +1,5 @@
+﻿$RepositoryRoot = Split-Path $PSScriptRoot | Split-Path
+Push-Location -Path $RepositoryRoot
+$Env:GITHUB_REPOSITORY = "ChaoticOnyx/OnyxBay"
+dotnet script "$PSScriptRoot\Fetch.csx"
+Pop-Location
