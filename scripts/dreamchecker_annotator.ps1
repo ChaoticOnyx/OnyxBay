e4804e9d1c4c108923e61b0ab8ffaf03dc0c0bd4
Igor Spichkin <igorsa.ux@outlook.com>
2021-05-24 23:40:58 +0300

    final

--- scripts/dreamchecker_annotator.ps1
+++ scripts/dreamchecker_annotator.ps1
@@ -1,3 +1,20 @@
+# Скрипт создаёт аннотации из вывода dreamchecker'а
+
+if ($null -eq $env:GITHUB_WORKSPACE)
+{
+    throw 'Переменная среды GITHUB_WORKSPACE не найдена.'
+}
+
+if ($null -eq $env:GITHUB_REPOSITORY)
+{
+    throw 'Переменная среды GITHUB_REPOSITORY не найдена.'
+}
+
+if ($null -eq $env:PR)
+{
+    throw 'Переменная среды PR не найдена.'
+}
+
 $Bin = 'dreamchecker'
 
 if ($IsWindows)
@@ -5,15 +22,19 @@ if ($IsWindows)
     $Bin = $Bin + '.exe'
 }
 
-$RepositoryRoot = $PSScriptRoot | Split-Path -Parent
-$Bin = Join-Path -Path $RepositoryRoot $Bin
-$TargetOut = Join-Path -Path $RepositoryRoot 'annotations.json'
+$Bin = Join-Path -Path $env:GITHUB_WORKSPACE $Bin
+$TargetOut = Join-Path -Path $env:GITHUB_WORKSPACE 'annotations.json'
 
 if (-not(Test-Path $Bin))
 {
-    Write-Host "Файл $Bin не найден!"
-    return 1 | Out-Null
+    Write-Error "Файл $Bin не найден!"
 }
+
+# Получение списка изменённых файлов.
+$Response = Invoke-WebRequest -Uri "https://api.github.com/repos/$env:GITHUB_REPOSITORY/pulls/$env:PR/files" -Headers @{ Accept = 'application/vnd.github.groot-preview+json' } `
+            -Method 'GET' -AllowUnencryptedAuthentication -SslProtocol 'Tls12'
+
+$ChangedFiles = $Response.Content | ConvertFrom-Json
 
 $ProcessInfo = New-Object System.Diagnostics.ProcessStartInfo
 $ProcessInfo.FileName = $Bin
@@ -43,6 +64,13 @@ $ResultJson = @()
 
 foreach ($match in $RegexMatches.Matches)
 {
+    $Path = $match.Groups | Where-Object -Property 'name' -eq 'filename' | Select-Object -ExpandProperty 'Value'
+
+    if ($null -eq ($ChangedFiles | Where-Object -Property 'filename' -eq $Path))
+    {
+        continue
+    }
+
     $Column = ($match.Groups | Where-Object -Property 'name' -eq 'column' | Select-Object -ExpandProperty 'Value') -as [int]
     $Line = ($match.Groups | Where-Object -Property 'name' -eq 'line' | Select-Object -ExpandProperty 'Value') -as [int]
     $Level = $match.Groups | Where-Object -Property 'name' -eq 'type' | Select-Object -ExpandProperty 'Value'
@@ -54,13 +82,13 @@ foreach ($match in $RegexMatches.Matches)
 
     $ResultJson += @{
         message = $match.Groups | Where-Object -Property 'name' -eq 'message' | Select-Object -ExpandProperty 'Value'
-        path = $match.Groups | Where-Object -Property 'name' -eq 'filename' | Select-Object -ExpandProperty 'Value'
+        path = $Path
         start_line = $Line
         end_line = $Line
         start_column = $Column
         end_column = $Column
         annotation_level = $Level
-        title = 'DreamChecker'
+        title = $Level
     }
 }
 
--- scripts/dreamchecker_downloader.ps1
+++ scripts/dreamchecker_downloader.ps1
@@ -1,6 +1,10 @@
 # Скрипт загружает последнюю доступную версию dreamchecker'a с гитхаба в корень репозитория.
 
-$RepositoryRoot = $PSScriptRoot | Split-Path -Parent
+if ($null -eq $env:GITHUB_WORKSPACE)
+{
+    throw 'Переменная среды GITHUB_WORKSPACE не найдена.'
+}
+
 $TargetBin = 'dreamchecker'
 
 if ($IsWindows)
@@ -8,18 +12,17 @@ if ($IsWindows)
     $TargetBin = $TargetBin + '.exe'
 }
 
-$TargetPath = Join-Path -Path $RepositoryRoot $TargetBin
+$TargetPath = Join-Path -Path $env:GITHUB_WORKSPACE $TargetBin
 $Headers = @{ Accept = 'application/vnd.github.groot-preview+json' }
 
 $Response = Invoke-WebRequest -Uri 'https://api.github.com/repos/SpaceManiac/SpacemanDMM/releases/latest' `
             -Headers $Headers -Method Get
 
 $Result = $Response.Content | ConvertFrom-Json
-$Asset = $Result.assets | Where-Object -Property 'name' -eq $TargetBin | Select-Object
+$Asset = $Result.assets | Where-Object -Property 'name' -eq $TargetBin | Select-Object -ExpandProperty 'browser_download_url'
 
+Write-Host "Загрузка $Asset в $TargetPath"
 
-Write-Host "Загрузка $($Asset.browser_download_url)"
-
-$Response = Invoke-WebRequest -Uri $Asset.browser_download_url -Headers $Headers `
+$Response = Invoke-WebRequest -Uri $Asset -Headers $Headers `
             -Method 'GET' -AllowUnencryptedAuthentication -OutFile $TargetPath `
-            -SslProtocol 'Tls12' & Write-Host "Загружено в $TargetPath"
+            -SslProtocol 'Tls12'
