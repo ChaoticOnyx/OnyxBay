819be7f7dde02f1f8844bef808590da22ac83092
Filatelele <dfilatoff2014@yandex.ru>
2024-01-04 20:01:42 +0300

    TGUI redone and i'm undone

--- tgui/packages/tgui/interfaces/OreRedemptionMachine.tsx
+++ tgui/packages/tgui/interfaces/OreRedemptionMachine.tsx
@@ -35,59 +35,51 @@ type InputData = {
 export const OreRedemptionMachine = (props: any, context: any) => {
   const { act, data } = useBackend<InputData>(context);
   return (
-    <Window title="Ore Redemption Machine" width={435} height={500}>
+    <Window title="Ore Redemption Machine" width={435} height={435}>
       <Window.Content>
-        <Stack fill vertical>
-          <Section>
-            <Stack.Item>
-              <Section>
-                <Stack>
-                  <Stack.Item>
-                    <Icon
-                      name="id-card"
-                      size={3}
-                      mr={1}
-                      color={data.user ? 'green' : 'red'}
-                    />
-                  </Stack.Item>
-                  <Stack.Item>
-                    <LabeledList>
-                      <LabeledList.Item label="Name">
-                        {data.user?.name || 'No Name Detected'}
-                      </LabeledList.Item>
-                      <LabeledList.Item label="Balance">
-                        {data.user?.cash || 'No Balance Detected'}
-                      </LabeledList.Item>
-                    </LabeledList>
-                  </Stack.Item>
-                </Stack>
-              </Section>
-            </Stack.Item>
-          </Section>
-          <Section>
-            <Stack.Item>
-              <Box>
-                <Icon name="coins" color="gold" />
-                <Box inline color="label" ml={1}>
-                  Unclaimed points:
-                </Box>
-                {data.unclaimedPoints}
-                <Button
-                  ml={2}
-                  content="Claim"
-                  disabled={data.unclaimedPoints === 0}
-                  onClick={() => act('Claim')}
-                />
+        <Stack fill vertical justify="space-between">
+          <Stack.Item>
+            <Section>
+              <Stack>
+                <Stack.Item>
+                  <Icon
+                    name="id-card"
+                    size={3}
+                    mr={1}
+                    color={data.user ? 'green' : 'red'}
+                  />
+                </Stack.Item>
+                <Stack.Item>
+                  <LabeledList>
+                    <LabeledList.Item label="Name">
+                      {data.user?.name || 'No Name Detected'}
+                    </LabeledList.Item>
+                    <LabeledList.Item label="Balance">
+                      {data.user?.cash || 'No Balance Detected'}
+                    </LabeledList.Item>
+                  </LabeledList>
+                </Stack.Item>
+              </Stack>
+            </Section>
+          </Stack.Item>
+          <Stack.Item>
+            <Section>
+              <Icon name="coins" color="gold" />
+              <Box inline color="label" ml={1}>
+                Unclaimed points:
               </Box>
-            </Stack.Item>
-          </Section>
+              {data.unclaimedPoints}
+              <Button
+                ml={2}
+                content="Claim"
+                disabled={data.unclaimedPoints === 0}
+                onClick={() => act('Claim')}
+              />
+            </Section>
+          </Stack.Item>
           <Stack.Item grow>
-            <Section fill scrollable>
-              {data.materials.length ? (
-                MaterialsList(data.materials, context)
-              ) : (
-                <NoticeBox>No materials data found!</NoticeBox>
-              )}
+            <Section fill scrollable justify="space-between">
+              <MaterialsList />
             </Section>
           </Stack.Item>
         </Stack>
@@ -96,41 +88,37 @@ export const OreRedemptionMachine = (props: any, context: any) => {
   );
 };
 
-function MaterialsList (props: Material[], context: any) {
-  const { act } = useBackend<InputData>(context);
+function MaterialsList (props: any, context: any) {
+  const { act, data } = useBackend<InputData>(context);
   return (
-    <Stack.Item grow>
-      <Section fill scrollable>
-        <Table>
-          {props.map(material => (
-            <Table.Row className="candystripe" collapsing>
-            <Table.Cell>{toTitleCase(material.name)}</Table.Cell>
-            <Table.Cell collapsing textAlign="left">
-            <Button
-                content="no action"
-                color="red"
-                onClick={() => act("no_act")}
-              />
-              <Button
-                content="smelt"
-                color="red"
-                onClick={() => act("no_act")}
-              />
-              <Button
-                content="compress"
-                color="red"
-                onClick={() => act("no_act")}
-              />
-              <Button
-                content="alloy"
-                color="red"
-                onClick={() => act("no_act")}
-              />
-            </Table.Cell>
-          </Table.Row>
-          ))}
-        </Table>
-      </Section>
-    </Stack.Item>
+    <Table>
+      {data.materials.map(material => (
+        <Table.Row className="candystripe" collapsing>
+        <Table.Cell>{toTitleCase(material.name)}</Table.Cell>
+        <Table.Cell collapsing textAlign="left">
+        <Button
+            content="no action"
+            color="red"
+            onClick={() => act("no_act")}
+          />
+          <Button
+            content="smelt"
+            color="red"
+            onClick={() => act("no_act")}
+          />
+          <Button
+            content="compress"
+            color="red"
+            onClick={() => act("no_act")}
+          />
+          <Button
+            content="alloy"
+            color="red"
+            onClick={() => act("no_act")}
+          />
+        </Table.Cell>
+      </Table.Row>
+      ))}
+    </Table>
   );
 }
