23fe0dc1f121c7a8374d34c18eb554a4da170c5e
Filatelele <dfilatoff2014@yandex.ru>
2024-03-17 13:29:31 +0300

    tweak(secure storage): removes excess of nanoui

---
+++ tgui/packages/tgui/interfaces/LockedSafe.tsx
@@ -0,0 +1,55 @@
+import { BooleanLike } from "common/react";
+
+import { useBackend } from "../backend";
+import { Box, Stack } from "../components";
+import { Window } from "../layouts";
+
+import { LockKeypad } from "./common/LockKeypad";
+
+export interface LockData {
+  input_code: string;
+  locked: BooleanLike;
+  lock_code: BooleanLike;
+  emagged: BooleanLike;
+  lock_setshort: BooleanLike;
+}
+
+export const LockedSafe = (props: any, context: any) => {
+  const { act, data } = useBackend<LockData>(context);
+  const { input_code, locked, lock_code, emagged, lock_setshort } = data;
+  return (
+    <Window width={300} height={400}>
+      <Window.Content>
+        <LockMenu></LockMenu>
+      </Window.Content>
+    </Window>
+  );
+};
+
+export const LockMenu = (props: any, context: any) => {
+  const { act, data } = useBackend<LockData>(context);
+  const { input_code, locked, lock_code, emagged, lock_setshort } = data;
+
+  return (
+    <Box m="6px">
+      <Box mb="6px" className="NuclearBomb__displayBox">
+        {input_code}
+      </Box>
+      <Box className="NuclearBomb__displayBox">
+        {!lock_code && !emagged && !lock_setshort ? "No password set." : ""}
+        {emagged && !lock_setshort ? "SYSTEM ERROR 1701" : ""}
+        {lock_setshort ? "SYSTEM ERROR 6040" : ""}
+        {!!lock_code && !emagged && !lock_setshort
+          ? locked
+            ? "Locked"
+            : "Unlocked"
+          : ""}
+      </Box>
+      <Stack ml="48px" mt="12px">
+        <Stack.Item>
+          <LockKeypad />
+        </Stack.Item>
+      </Stack>
+    </Box>
+  );
+};
---
+++ tgui/packages/tgui/interfaces/common/LockKeypad.tsx
@@ -0,0 +1,46 @@
+import { classes } from "common/react";
+
+import { useBackend } from "../../backend";
+import { Box, Button, Grid } from "../../components";
+
+// This ui is so many manual overrides and !important tags
+// and hand made width sets that changing pretty much anything
+// is going to require a lot of tweaking it get it looking correct again
+// I'm sorry, but it looks bangin
+export const LockKeypad = (props: any, context: any) => {
+  const { act } = useBackend(context);
+  const keypadKeys = [
+    ["1", "4", "7", "C"],
+    ["2", "5", "8", "0"],
+    ["3", "6", "9", "E"],
+  ];
+  return (
+    <Box width="185px">
+      <Grid width="1px">
+        {keypadKeys.map((keyColumn) => (
+          <Grid.Column key={keyColumn[0]}>
+            {keyColumn.map((key) => (
+              <Button
+                fluid
+                bold
+                key={key}
+                mb="6px"
+                content={key}
+                textAlign="center"
+                fontSize="40px"
+                lineHeight={1.25}
+                width="55px"
+                className={classes([
+                  "NuclearBomb__Button",
+                  "NuclearBomb__Button--keypad",
+                  "NuclearBomb__Button--" + key,
+                ])}
+                onClick={() => act("keypad", { digit: key })}
+              />
+            ))}
+          </Grid.Column>
+        ))}
+      </Grid>
+    </Box>
+  );
+};
